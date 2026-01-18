04b0718538d9fad3003621fe6fdb9acd97dd81b5
Carolina785 <cfeliciano178@gmail.com>
2026-01-18 14:52:51 -0300

    código finalizado

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// Função que calcula o saldo de vitórias e determina o nível do jogador
 function calculadoraPartidas(vitorias, derrotas) {
+    //Calcula o saldo de vitórias (vitórias menos derrotas)
     let saldoVitorias = vitorias - derrotas;
+    // Variável para armazenar o nível do jogador
     let nivel = ""
 
+    // Estrutura condicional para definir o nível baseado no número de vitórias
     if(vitorias < 10) {
         nivel = "Ferro";
     } else if(vitorias <= 20) {
@@ -18,13 +22,15 @@ function calculadoraPartidas(vitorias, derrotas) {
         nivel = "Imortal";
     }
 
+    // Retorna um objeto com o saldo de vitórias e o nível
     return {
         saldoVitorias: saldoVitorias,
         nivel: nivel
     };
 }
 
-
+//Chama a função passando 140 vitórias e 40 derrotas e guarda o resultado
 let resultado = calculadoraPartidas(140, 40);
 
+// Exibe a mensagem final formatada com o saldo e o nível do jogador
 console.log(`O Herói tem de saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`);
